36d1d9f0f754c4df970ff9d7dc1f07b97c67bb8b
MashaqRamadhan <mashaqramadhanmashaq@gmail.com>
2024-12-20 09:15:12 +0800

    fisrt commit

---
+++ UTS/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# Classification with Random Forest
+    # y_pred_rf = Random_forest(X_train, y_train, X_test)
+    # print("Random Forest Classification Report:")
+    # print(classification_report(y_test, y_pred_rf))
+    # display_confusion_matrix(y_test, y_pred_rf, "Random Forest Confusion Matrix")
